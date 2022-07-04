ac20dc6ce0bc3da31cf0307904c9e3187a02878e
Alex Monteiro <alex.monteiro@ifam.edu.br>
2022-07-04 10:37:36 -0500

    exemplo02 da apostila

--- exemplo02/exemplo2.pas
+++ exemplo02/exemplo2.pas
@@ -5,15 +5,18 @@ unit exemplo2;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    btCalcular: TButton;
     bbSair: TButton;
+    BitBtn1: TBitBtn;
+    procedure bbSairClick(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
+    procedure btCalcularClick(Sender: TObject);
   private
 
   public
@@ -26,6 +29,39 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.btCalcularClick(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.BitBtn1Click(Sender: TObject);
+var
+   s_altura, s_base, s_area : string;
+   area, base, altura : double;
+begin
+     //recuperação de variavéis
+     s_altura := inputBox('Digite a altura','Valor: ','');
+     s_base := inputBox('Digite a base','Valor: ','');
+
+     //processamento
+     base := StrToFloat(s_base);
+     altura := StrToFloat(s_altura);
+
+     area := base*altura/2;
+
+     s_area := FloatToStr(area);
+
+     //saída
+     ShowMessage('A área do triangulo é: ' + s_area);
+end;
+
+procedure TForm1.bbSairClick(Sender: TObject);
+begin
+  close;
+end;
 
 end.
 
